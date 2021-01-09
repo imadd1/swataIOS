72a23a02054909c89d489bb937645b86ad8fba63
imad damianos <imad-damianos@hotmail.com>
2021-01-09 23:24:28 +0200

    Initial Commit

---
+++ swata/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swata
+//
+//  Created by imps on 1/9/21.
+//  Copyright © 2021 imps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
